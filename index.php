d9c84de8aaa62b5b7865542e69d2870f797aedd3
patrickpoort <patrickpoort@gmail.com>
2017-01-04 21:15:40 +0100

    and

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 <?php include "HTML HEAD.php"; ?>
 
 <html>
-<<<<<<< HEAD
-<h1>
-    Hello
+<h1> Hello Guys
 </h1>
-<p>Wat gaan we doen poorty</p>
-<p>poooooooooooooooootrzzz</p>
-=======
-<<<<<<< HEAD
-<body>
-
-<h1>Hello test enviroment</h1>
 
 
 
-
-</body>
-
->>>>>>> origin/master
 </html>
 <?php include "footer.php"; ?>
 
