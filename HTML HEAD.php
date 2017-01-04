129268df43ba846e4e24eab22ffe6bbb29c0999f
patrickpoort <patrickpoort@gmail.com>
2017-01-04 21:29:43 +0100

    main css

--- HTML HEAD.php
+++ HTML HEAD.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap-3.3.7-dist/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
