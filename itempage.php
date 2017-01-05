cec2a20dd275126b87573e2047d857662a7e0e42
Bas Zandvliet <bas.zandvliet97@gmail.com>
2017-01-05 15:10:32 +0100

    itempage HTML HEAD toegevoegd

--- itempage.php
+++ itempage.php
@@ -1,23 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>cpanel - klanten</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- Custom styles for this template -->
-    <!--    <link href="css/main.css" rel="stylesheet">-->
-</head>
+<html>
+
+    
 <body>
 <?php
 session_start();
 
 // navbar include
-include("include/navigation.php");
+include("HTML HEAD.php");
 
 // database include
 include 'include/database.php';
