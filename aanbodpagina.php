d7d090f56f34f8f09c4f3fcc9ad89a01633504d3
Bas Zandvliet <bas.zandvliet97@gmail.com>
2017-01-05 15:06:06 +0100

    aanbodpagina footer toegevoegd

--- aanbodpagina.php
+++ aanbodpagina.php
@@ -160,7 +160,7 @@
     </div>
 </div>
 
-<?php include("include/Footer.php"); ?>
+<?php include("footer.php"); ?>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="css/https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
