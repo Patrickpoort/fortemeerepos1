c1671295a44f8fc0640cdd56e16aae105b1b8c0a
cdokkum <Cor@84-207.dhcp.windesheim.nl>
2017-01-09 10:30:08 +0100

    images en navaar aangepast

--- Navbar.php
+++ Navbar.php
@@ -29,7 +29,7 @@
                 <?php
                 // Geeft weer als iemand ingelogd is en anders de optie om te registreren.
                 if (isset($_SESSION['emailadres'])) {
-                    print "<li><a href='profilepage.php'>" . $_SESSION['emailadres'] . "</a></li>";
+                    print "<li><a href='profilepage.php'>" . "U bent ingelogd als " . $_SESSION['emailadres'] . "</a></li>";
                     print "<li><a href='logout.php'><span>Logout</span></a></li>";
                 } else {
                     print"<li><a href='login.php'><span>Login</span></a></li>";
